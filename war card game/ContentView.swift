61aa89317f2b252bd5e04e1f44217064bf59d198
SamsShow <sakshamtyagi2008@gmal.com>
2025-08-19 05:49:41 +0530

    UI Update

--- war card game/ContentView.swift
+++ war card game/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            
+            Image("background-plain").resizable().edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Image("card3")
+                    Image("card4 ")
+                }
+                Spacer()
+                Image("button")
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack{
+                        Text("Player").font(.headline).padding(.bottom, 10.0)
+                        Text("0").font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU").font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0").font(.largeTitle)
+                    }
+                    Spacer()
+                }.foregroundStyle(Color.white)
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
