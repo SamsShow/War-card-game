c47dbde5ff1bc7c99eea4ec1d3f8982b540dc5dd
SamsShow <sakshamtyagi2008@gmal.com>
2025-08-20 06:44:01 +0530

    function added

--- war card game/ContentView.swift
+++ war card game/ContentView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "back"
+    @State var cpuCard = "back"
+    
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
         ZStack{
             
@@ -18,29 +25,54 @@ struct ContentView: View {
                 Image("logo")
                 Spacer()
                 HStack {
-                    Image("card3")
-                    Image("card4 ")
+                    Image(playerCard)
+                    Image(cpuCard)
                 }
                 Spacer()
-                Image("button")
+//                Image("button")
+                
+                Button{
+                    deal()
+                } label: {
+                    Image("button")
+                }
+                
                 Spacer()
                 HStack {
                     Spacer()
                     VStack{
                         Text("Player").font(.headline).padding(.bottom, 10.0)
-                        Text("0").font(.largeTitle)
+                        Text(String(playerScore)).font(.largeTitle)
                     }
                     Spacer()
                     VStack{
                         Text("CPU").font(.headline)
                             .padding(.bottom, 10.0)
-                        Text("0").font(.largeTitle)
+                        Text(String(cpuScore)).font(.largeTitle)
                     }
                     Spacer()
                 }.foregroundStyle(Color.white)
                 Spacer()
             }
         }
+    }
+    func deal(){
+        // Randomize the player card
+        var playerCardIndex = Int.random(in: 2...13)
+        playerCard = "card" + String(playerCardIndex)
+        
+        // Rnadomize the CPU card
+        var cpuCardIndex = Int.random(in: 2...13)
+        cpuCard = "card" + String(cpuCardIndex)
+        
+        // Update the Score
+        if playerCardIndex > cpuCardIndex {
+            playerScore += 1
+        }
+        else if playerCardIndex < cpuCardIndex {
+            cpuScore += 1
+        }
+        
     }
 }
 
